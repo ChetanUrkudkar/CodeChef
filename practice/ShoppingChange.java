e736e19f7562f55716ba05a9e9dffecc6c7c64d8
Chetan Urkudkar <84468525+ChetanUrkudkar@users.noreply.github.com>
2023-01-11 00:42:09 +0530

    Add files via upload

---
+++ practice/ShoppingChange.java
@@ -0,0 +1,18 @@
+// Link : https://www.codechef.com/problems/SHOPCHANGE
+
+package practice;
+
+import java.util.*;
+public class ShoppingChange {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int t = input.nextInt();
+		
+		while (t --> 0) {
+			int x = input.nextInt();
+			
+			System.out.println(100-x);
+		}
+	}
+}
