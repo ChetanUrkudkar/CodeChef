a36dd7e3c61edb71350a80884ccd22e87414ad61
Chetan Urkudkar <84468525+ChetanUrkudkar@users.noreply.github.com>
2023-10-24 14:52:31 +0530

    Add files via upload

---
+++ beginnerDSA/Fitness.java
@@ -0,0 +1,16 @@
+// Link : https://www.codechef.com/practice/PJABM01/problems/FIT
+
+package beginnerDSA;
+import java.util.*;
+
+public class Fitness {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int t = input.nextInt();
+		
+		while (t --> 0) {
+			int x = input.nextInt();
+			System.out.println((x * 2) * 5);
+		}
+	}
+}
